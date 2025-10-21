d2f09149311ab306152b4889ade19bf6b6df50d1
Sanjh Dey Sarker <sanjhdeysarker@gmail.com>
2025-10-21 17:09:09 +0530

    user_service-change

--- run_broker.py
+++ run_broker.py
@@ -1,20 +1,4 @@
-import socket, json, threading
+from pubsub.broker import start_broker
 
-class PubSubClient:
-    def __init__(self, name, host="127.0.0.1", port=9000):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((host, port))
-        self.name = name
-        self.sock.send(json.dumps({"action":"register","service":name}).encode())
-        threading.Thread(target=self.listen, daemon=True).start()
-
-    def listen(self):
-        while True:
-            data = self.sock.recv(4096)
-            if not data: break
-            msg = json.loads(data.decode())
-            print(msg)
-
-    def subscribe(self, topic): self.sock.send(json.dumps({"action":"subscribe","topic":topic}).encode())
-    def publish(self, topic, msg): self.sock.send(json.dumps({"action":"publish","topic":topic,"message":msg}).encode())
-    def send_message(self, to, msg): self.sock.send(json.dumps({"action":"message","to":to,"message":msg}).encode())
+if __name__ == "__main__":
+    start_broker()
