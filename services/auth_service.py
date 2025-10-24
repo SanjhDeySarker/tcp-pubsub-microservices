c3200c0734efe19dd936a433be2512d0f86e9554
Sanjh Dey Sarker <sanjhdeysarker@gmail.com>
2025-10-24 16:58:57 +0530

    new feature modification

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,20 +1,15 @@
 from pubsub.client import PubSubClient
-import time
 
+def on_user_created(message, sender):
+    print(f"[AUTH SERVICE] Got event from {sender}: {message}")
 
-def on_user_created(topic, message):
-    print(f"[EVENT] New {topic} from user-service: {message}")
+# Only accept users with ID > 100
+def filter_user(msg):
+    return msg.get("user_id", 0) > 100
 
+client = PubSubClient("auth-service")
+client.subscribe("user_created", callback=on_user_created, filter_func=filter_user)
 
-if __name__ == "__main__":
-    client = PubSubClient("auth-service")
-
-    # Subscribe to user_created events
-    client.subscribe("user_created", on_user_created)
-
-    print("✅ auth-service running. Listening for messages...")
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        client.close()
+print("✅ auth-service running. Listening for messages...")
+while True:
+    pass
