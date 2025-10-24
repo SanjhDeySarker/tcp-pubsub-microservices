a570fd1d3f65e348911badaa7429f9e16a904053
Sanjh Dey Sarker <sanjhdeysarker@gmail.com>
2025-10-24 14:46:25 +0530

    auth-service-changes

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,14 +1,20 @@
 from pubsub.client import PubSubClient
 import time
 
-def on_user_created(message, sender):
-    print(f"[EVENT] New user event from {sender}: {message}")
 
-client = PubSubClient("auth-service")
+def on_user_created(topic, message):
+    print(f"[EVENT] New {topic} from user-service: {message}")
 
-# Subscribe to topic with callback
-client.subscribe("user_created", on_user_created)
 
-print("✅ auth-service running. Listening for messages...")
-while True:
-    time.sleep(1)
+if __name__ == "__main__":
+    client = PubSubClient("auth-service")
+
+    # Subscribe to user_created events
+    client.subscribe("user_created", on_user_created)
+
+    print("✅ auth-service running. Listening for messages...")
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        client.close()
